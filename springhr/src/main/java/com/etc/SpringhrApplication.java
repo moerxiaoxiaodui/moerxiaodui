70f91ec0c9fc552f1b62150ae2be1d06438980cc
haoge250 <2677800147@qq.com>
2020-10-20 15:08:59 +0800

    huge

--- springhr/src/main/java/com/etc/SpringhrApplication.java
+++ springhr/src/main/java/com/etc/SpringhrApplication.java
@@ -2,8 +2,10 @@ package com.etc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringhrApplication {
 
     public static void main(String[] args) {
--- springuser/src/main/java/com/etc/SpringuserApplication.java
+++ springuser/src/main/java/com/etc/SpringuserApplication.java
@@ -2,8 +2,10 @@ package com.etc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringuserApplication {
 
     public static void main(String[] args) {
