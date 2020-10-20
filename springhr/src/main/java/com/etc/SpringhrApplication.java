dc41f9767266d62361b045d592e9c0604b0c11c2
haoge250 <2677800147@qq.com>
2020-10-20 14:40:04 +0800

    huge

---
+++ springhr/src/main/java/com/etc/SpringhrApplication.java
@@ -0,0 +1,13 @@
+package com.etc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringhrApplication.class, args);
+    }
+
+}
