73326f70589764fd92f6a734fb52290b6945527d
haoge250 <2677800147@qq.com>
2020-10-22 20:03:10 +0800

    huge

---
+++ springmanager/src/main/java/com/etc/SpringmanagerApplication.java
@@ -0,0 +1,15 @@
+package com.etc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmanagerApplication.class, args);
+    }
+
+}
