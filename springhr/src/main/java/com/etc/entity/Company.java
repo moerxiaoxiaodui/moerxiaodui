e893d3933a8ef5479b226d7d6ecb32947448bd08
chenmingzhou1 <1535368918@qq.com>
2020-10-24 10:33:23 +0800

    cmz3

--- springhr/src/main/java/com/etc/entity/Company.java
+++ springhr/src/main/java/com/etc/entity/Company.java
@@ -1,11 +1,27 @@
 package com.etc.entity;
 
+import com.sun.xml.internal.ws.developer.UsesJAXBContext;
 import lombok.Data;
 
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 @Table(name = "company")
 @Data
+@Entity
 public class Company implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name="name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+    @Column(name="avatar")
+    private String avatar;
+    @Column(name = "introduce")
+    private String introduce;
+    @Column(name = "scale")
+    private String scale;
+    @Column(name = "type")
+    private String type;
 }
--- springhr/src/main/java/com/etc/service/HRService.java
+++ springhr/src/main/java/com/etc/service/HRService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface HRService {
 
     public List<HR> findAll();
+    public HR findHrByPhone(String phone);
+    public HR findHrByPhoneAndPassword(String phone,String password);
 }
